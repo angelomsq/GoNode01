9545599e9cc5334602d72af7d84b60a3bb9d504d
Angelo Queiroz <angeloqueiroz@MacBook-Pro-de-Angelo.local>
2018-07-20 17:16:12 -0300

    add .eslintrc

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const moment = require('moment');
 const app = express();
 
 nunjucks.configure('views', {
-    autoescape: true,
-    express: app
+  autoescape: true,
+  express: app,
 });
 
 app.set('view engine', 'njk');
@@ -16,35 +16,33 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   res.render('main');
 });
 
-app.post('/check', (req,res) => {
-  const nome = req.body.nome;
-  const nascimento = req.body.nascimento;
-  const idade = moment().diff(moment(nascimento,"YYYY-MM-DD"), 'years');
-  console.log(idade);
-  if(idade >=18){
-    res.redirect('/major?nome='+nome);
-  }else{
-    res.redirect('/minor?nome='+nome);
+app.post('/check', (req, res) => {
+  const { nome, nascimento } = req.body;
+  const idade = moment().diff(moment(nascimento, 'YYYY-MM-DD'), 'years');
+  if (idade >= 18) {
+    res.redirect(`/major?nome=${nome}`);
+  } else {
+    res.redirect(`/minor?nome=${nome}`);
   }
 });
 
 const ageMiddleware = (req, res, next) => {
-  if(req.query.nome){
+  if (req.query.nome) {
     next();
-  }else{
+  } else {
     res.redirect('/');
   }
-}
+};
 
-app.get('/major', ageMiddleware, (req,res) => {
+app.get('/major', ageMiddleware, (req, res) => {
   res.render('major', req.query);
 });
 
-app.get('/minor', ageMiddleware, (req,res) => {
+app.get('/minor', ageMiddleware, (req, res) => {
   res.render('minor', req.query);
 });
 
